1322f9feeaf5b2f740f46beb7ba963b0eaa8384c
Bira Lavor <biralavor@gmail.com>
2025-03-14 22:53:33 -0300

    Add one more object as a client class

--- Aula-03/ClientProject/src/br/com/fiap/main/TesteCliente.java
+++ Aula-03/ClientProject/src/br/com/fiap/main/TesteCliente.java
@@ -4,17 +4,21 @@ import br.com.fiap.modelo.Cliente;
 
 public class TesteCliente {
     public static void main(String[] args) {
-        Cliente objCliente = new Cliente(44, "Bira", "rm565463@fiap.com.br", 10);
+        Cliente objCliente1 = new Cliente(44, "Bira", "rm565463@fiap.com.br", 10);
+        Cliente objCliente2 = new Cliente(18, "Lucas", "rm1234@fiap.com.br", 12);
 
         // Using methods
-        objCliente.displayClientInfo();
+        objCliente1.displayClientInfo();
 
         //Modifying attributes
-        objCliente.setAge(15);
-        objCliente.setEmail("bira@github.com");
-        objCliente.setIncome(1.305f);
-        objCliente.setName("Aluno");
+        System.out.println("Updating last client");
+        objCliente1.setAge(15);
+        objCliente1.setEmail("bira@github.com");
+        objCliente1.setIncome(1.305f);
+        objCliente1.setName("Bira Lavor");
 
-        objCliente.displayClientInfo();
+        objCliente1.displayClientInfo();
+        System.out.println("Adding new client");
+        objCliente2.displayClientInfo();
     }
 }
