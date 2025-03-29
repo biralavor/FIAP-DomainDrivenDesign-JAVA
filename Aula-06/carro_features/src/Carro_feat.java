ffbd6232cc6f984c3f225c70ae6829d13fcd156e
Bira Lavor <biralavor@gmail.com>
2025-03-28 23:56:03 -0300

    add aula 06

---
+++ Aula-06/carro_features/src/Carro_feat.java
@@ -0,0 +1,13 @@
+public class Carro_feat {
+    private boolean status;
+    private int speed;
+    private float fuel;
+    private String model;
+    private String brand;
+
+
+    //Constructor method
+    public Carro_feat(boolean status, int speed, float fuel, String model, String brand){
+        
+    }
+}
