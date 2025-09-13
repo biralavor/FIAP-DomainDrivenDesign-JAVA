98ee75b6b903954faa68a81ec92788c1ed889ecc
Bira Lavor <biralavor@gmail.com>
2025-09-12 23:06:35 -0300

    feat insert into table - CRUD

--- Semestre02/Aula20-DBconnection/src/mvc/model/dao/ClientConnection.java
+++ Semestre02/Aula20-DBconnection/src/mvc/model/dao/ClientConnection.java
@@ -1,5 +1,9 @@
 package mvc.model.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import mvc.model.vo.ClientVO;
 import mvc.model.vo.Credentials;
 
@@ -50,6 +54,83 @@ public class ClientConnection {
     private void tableDDLPrinter(String tableName) {
         System.out.println("\n---- " + tableName + " DDL ----\n");
 
+    }
+
+    public boolean insertIntoTable(ClientVO newClient, String tableName) {
+        String query = "INSERT INTO" + tableName + "VALUES (?, ?, ?)";
+        try {
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setInt(1, newClient.getId());
+            ps.setString(2, newClient.getFirstName());
+            ps.setString(3, newClient.getLastName());
+            ps.execute();
+        } catch (Exception e) {
+            if (conn == null)
+                System.out.println("Error. Connection is Null.");
+            else
+                System.out.println("Error in PreparedStatement.");
+            e.fillInStackTrace();
+            return false;
+        } finally {
+            System.out.println("Closing connection...");
+            try {
+                assert conn != null;
+                conn.close();
+            } catch (SQLException e) {
+                System.out.println("Error while closing connection");
+                e.fillInStackTrace();
+            }
+        }
+        return true;
+    }
+
+    public boolean deleteIntoTable(int idx, String tableName, String column) {
+        String query = "DELETE FROM " + tableName + "WHERE " + column + " = ?";
+        try {
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setInt(1, idx);
+            ps.execute();
+        } catch (SQLException e) {
+            if (conn == null)
+                System.out.println("Error. Connection is Null.");
+            System.out.println("Error while deleting " + column + " at " + tableName + " table.");
+            e.fillInStackTrace();
+            return false;
+        } finally {
+            System.out.println("Closing connection");
+            try {
+                assert conn != null;
+                conn.close();
+            } catch (Exception e) {
+                e.fillInStackTrace();
+            }
+        }
+        return true;
+    }
+
+    public boolean updateIntoTable(int idx, String tableName, String column, String newString ) {
+        String query = "";
+        return true;
+    }
+
+    public List<ClientVO> readFromTable (String tableName) {
+        List<ClientVO> clientList = new ArrayList<ClientVO>();
+        String query = "SELECT * FROM " + tableName;
+        try {
+            PreparedStatement ps = conn.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()) {
+                int id = rs.getInt(1);
+                String firstName = rs.getString(2);
+                String lastName = rs.getString(3);
+                clientList.add(new ClientVO(id, firstName, lastName));
+            }
+        } catch (SQLException e) {
+            System.out.println("Error. Read from table error.");
+            e.fillInStackTrace();
+        }
+
+        return clientList;
     }
 
     public Connection getConnection() {
